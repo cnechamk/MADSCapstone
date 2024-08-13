1923ed12636b10dc9cf9901288fc2bc7edf92c90
Josh <joshfisher81@gmail.com>
2024-08-13 14:35:17 -0700

    moved PathLike to script

--- Scripts/preprocess_data.py
+++ Scripts/preprocess_data.py
@@ -1,8 +1,13 @@
 """ Various Preprocessing Scripts """
-import pandas as pd
-from sklearn.decomposition import PCA
-from sklearn.feature_extraction.text import TfidfVectorizer
+from typing import Union
+from pathlib import Path
 
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+
+PathLike = Union[str, Path]
 
 def parse_header(row):
     """ parse historical calendar data """
@@ -57,12 +62,39 @@ def fomc_impact(df_sp500, df_fomc, save_p):
     close = df_sp500.close
     df_fomc.date = pd.to_datetime(df_fomc.date)
 
-    def tmp(row):
-        og_idx = row['index']
-        idx = (row[0] - df_sp500.date).abs().idxmin()
-        val = (close.iloc[idx+1: idx+6].mean() - close.iloc[idx-5: idx].mean())
-        val_norm = val / close.iloc[idx-5: idx].mean()
-        return og_idx, val, val_norm
+    add_time_deltas = {
+        "1M": pd.to_timedelta(30, "D"),
+        "6M": pd.to_timedelta(30*6, "D"),
+        "1Y": pd.to_timedelta(365, "D"),
+        "3Y": pd.to_timedelta(365 * 3, "D"),
+        "5Y": pd.to_timedelta(365 * 5, "D")
+    }
+
+    def get_impacts(row):
+        def direct_impact(row):
+            og_idx = row['index']
+            idx = (row[0] - df_sp500.date).abs().idxmin()
+            val = (close.iloc[idx+1: idx+6].mean() - close.iloc[idx-5: idx].mean())
+            val_norm = val / close.iloc[idx-5: idx].mean()
+            return idx, val, val_norm
+
+        def indirect_impact(row, time_deltas=list[pd.Timedelta]):
+            return_vals = []
+            present_idx = (row[0] - df_sp500.date).abs().idxmin()
+            for time_delta in time_deltas:
+                offset_date = row[0] + time_delta
+                if offset_date > datetime.today():
+                    return_vals.extend([np.nan, np.nan])
+                    continue
+
+                offset_idx = (offset_date - df_sp500.date).abs().idxmin()
+                val = close.iloc[offset_idx] - close.iloc[present_idx]
+                val_norm = val / close.iloc[present_idx]
+                return_vals.extend([val, val_norm])
+
+            return return_vals
+
+        return (*direct_impact(row), *indirect_impact(row, add_time_deltas.values()))
 
     dates = pd.Series(
         df_fomc.date.unique()
@@ -70,10 +102,19 @@ def fomc_impact(df_sp500, df_fomc, save_p):
         ascending=False
     ).reset_index()
 
+    cols = {0: 'index', 1: "diff", 2: "diff_norm"}
+
+    i = 2
+    for offset in add_time_deltas.keys():
+        i += 1
+        cols[i] = f"{offset}_diff"
+        i += 1
+        cols[i] = f"{offset}_diff_norm"
+
     diffs = dates.apply(
-        tmp, axis=1, result_type="expand"
+        get_impacts, axis=1, result_type="expand"
     ).rename(
-        columns={0: 'index', 1: "diff", 2: "diff_norm"}
+        columns=cols
     )
 
     df = dates.merge(
@@ -144,28 +185,55 @@ def merge_beige_books_impact(df_beige_book: pd.DataFrame, df_fomc_impact: pd.Dat
 
     return df
 
-def pca(p_beige_books: str, p_fomc_impact: str, vectorizer: TfidfVectorizer):
-    """
-    5 components explained about 10% of variance
-    100 components explained about 30% of variance
-    1000 components explained
-    Args:
-        p_beige_books:
-        p_fomc_impact:
-        vectorizer:
 
-    Returns:
+def merge_beige_books_fed_rate(
+        df_beige_book: pd.DataFrame | PathLike,
+        df_fed_rate: pd.DataFrame | PathLike
+):
+    if isinstance(df_beige_book, PathLike):
+        df_beige_book = pd.read_csv(df_beige_book)
+        df_beige_book.date = pd.to_datetime(df_beige_book.date)
+    if isinstance(df_fed_rate, PathLike):
+        df_fed_rate = pd.read_csv(df_fed_rate)
+        df_fed_rate.date = pd.to_datetime(df_fed_rate.date)
 
-    """
-    df_beige_book = pd.read_csv(p_beige_books)
-    df_beige_book.date = pd.to_datetime(df_beige_book.date)
+    rows = []
+    df_beige_book = df_beige_book.reset_index()
+    df_fed_rate = df_fed_rate.reset_index()
 
-    df_fomc_impact = pd.read_csv(p_fomc_impact)
-    df_fomc_impact.date = pd.to_datetime(df_fomc_impact.date)
-    df_fomc_impact = df_fomc_impact.dropna()
+    time_delta = pd.to_timedelta(14., unit="D")
+    for bb_date in df_beige_book.date.unique():
+        est_im_date = bb_date + time_delta
+        date_diffs = (est_im_date - df_fed_rate.date).abs()
+        idxmin = date_diffs.idxmin()
+        impact = df_fed_rate.iloc[idxmin].to_dict()
+        impact['bb_date'] = bb_date
+        rows.append(impact)
 
-    df = merge_beige_books_impact(df_beige_book, df_fomc_impact)
+    tmp = pd.DataFrame.from_records(
+        rows
+    ).rename(
+        columns={'date': 'impact_date'}
+    )
 
-    text_vec = vectorizer.transform(df.text)
-    pca = PCA(1000)
-    x = pca.fit_transform(text_vec)
+    df = df_beige_book.merge(
+        tmp, left_on='date', right_on='bb_date'
+    ).drop(
+        columns=['date', 'index_x', 'index_y']
+    )
+
+    return df
+
+
+
+if __name__ == "__main__":
+    p_bb = "../Data/beige_books.csv"
+    p_fed = "../Data/fed_funds.csv"
+    p_fomc = "../Data/fomc_dates.csv"
+    p_sp500 = "../Data/sp500.csv"
+
+    df_sp500 = pd.read_csv(p_sp500)
+    df_fomc = pd.read_csv(p_fomc)
+
+    # merge_beige_books_fed_rate(p_bb, p_fed)
+    fomc_impact(df_sp500, df_fomc, False)
